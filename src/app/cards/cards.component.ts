1fbd84c477a22141696433c6ff08d7396c194fdf
Kübra EFE <kubraefe884@hotmail.com>
2023-10-16 01:00:18 +0300

    second
    bootstrap import, add list item card

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
   styleUrls: ['./cards.component.scss']
 })
-export class CardsComponent {
+export class CardsComponent  {
+
+
+  cardItem = {
+    title: 'Frontend',
+    name: 'Mehmet Sert',
+    phone: '0555 555 55 88',
+    email: 'info@mehmetsert.com',
+    address: 'Orhangazi, Bursa',
+  };
 
 }
